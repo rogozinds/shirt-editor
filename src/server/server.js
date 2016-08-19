dea411fb97bec0166c53aabfbadf197144e3c26d
Dmitrii Rogozin <dmitrii@vaadin.com>
2016-08-19 18:15:04 +0300

    Add server part for saving tasks in DB

--- src/server/server.js
+++ src/server/server.js
@@ -6,9 +6,11 @@
 // call the packages we need
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
-
+var PouchDB = require('pouchdb');
 var port = process.env.PORT || 3300;        // set our port
 //This is a dummy data should be normal db, right now just in memory
+var db = new PouchDB('http://localhost:5984/tasklist');
+
 var     data = [
     {
         "id" : "0",
@@ -33,6 +35,8 @@ var     data = [
     }
 ]
 
+
+
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
@@ -40,8 +44,10 @@ router.get('/tasklist', function(req, res) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers","Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-  //  res.send("Hell World");
-    res.json(data);
+    db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+        console.log(doc.rows);
+        res.json(doc.rows);
+    });
 });
 
 router.get('/tasks/:taskId', function(req,res){
@@ -50,8 +56,30 @@ router.get('/tasks/:taskId', function(req,res){
     res.setHeader("Access-Control-Allow-Headers","Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     //search in DB by ID
     console.log("GOT REQUEST");
-    res.send(data[req.params['taskId']]);
+    var id =req.params['taskId'];
+    db.get(id).then(function(result) {
+        res.send(result);
+    }).catch (function(err){
+        console.log(err);
+    });
+
 })
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+router.post('/tasks/add', function(req,res){
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers","Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    //search in DB by ID
+    var task=req.body;
+    db.post(task).then(function (doc) {
+    }).catch(function (err) {
+        console.log(err);
+    });
+    res.end();
+})
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
@@ -61,3 +89,5 @@ app.use('/api', router);
 // =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+
